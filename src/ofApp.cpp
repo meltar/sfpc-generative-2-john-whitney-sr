d632731095da05a53702b1e6cc74b7312e252eec
Melissa Holmes <meltar@users.noreply.github.com>
2019-03-31 02:38:12 -0400

    Bouncing triangles with coordinating circles

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -12,7 +12,35 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofBackground(38, 28, 29);
+    ofNoFill();
 
+    float xOrig = 400;
+    float yOrig = 400;
+    float radius = 200;
+    float angle = ofGetElapsedTimef();
+
+    float x = xOrig + radius * cos(angle*1);
+    float y = yOrig + radius * sin(angle*1.7);
+
+    ofSetColor(158, 166, 81);
+
+    for(int i = 0; i < 12; i++){
+        float x = ofMap(sin(angle*(i*0.1)), -1, 1, 0, ofGetWidth());
+        ofPolyline triangle;
+        triangle.addVertex(x,y+60);
+        triangle.addVertex(x-60,y-60);
+        triangle.addVertex(x+60,y-60);
+        triangle.close();
+        triangle.draw();
+    }
+
+    ofSetColor(97, 113, 108);
+
+    for (int i = 0; i < 12; i++){
+        float x = ofMap(sin(angle*(i*0.1)), -1, 1, 0, ofGetWidth());
+        ofDrawCircle(x,i*20,10);
+    }
 }
 
 //--------------------------------------------------------------
